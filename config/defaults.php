6f1b59f3224a27fe431134cef62a34ad84d5bf69
Sabrina Robinson <1763514@edu.vaniercollege.qc.ca>
2025-10-08 09:44:21 -0400

    updating my branch with the fixed version of main

--- config/defaults.php
+++ config/defaults.php
@@ -2,33 +2,48 @@
 
 declare(strict_types=1);
 
-// Application's default settings
+// Application's default settings.
 
-// Error reporting
+// Error reporting.
 // Default settings: disable all error reporting for production.
 error_reporting(0);
 ini_set('display_errors', '0');
 ini_set('display_startup_errors', '0');
-// Timezone
+
+// Timezone.
 date_default_timezone_set('America/Toronto');
 
 $settings = [];
 
-// Error handler
+// Error handler.
 $settings['error'] = [
     // Should be set to false for the production environment
     'display_error_details' => false,
 ];
 
-// Logger settings
+
+//TODO: Set the session path to a temporary directory.
+$settings['session'] = [
+    'name' => 'app_session',
+    'lifetime' => 30 * 557200,
+    //'path' => realpath(__DIR__ . '/../var/tmp'),
+    // Must be set to the application's base directory name.
+    'path' => '/' . APP_ROOT_DIR_NAME,
+    'domain' => 'localhost',
+    'secure' => false,
+    'httponly' => true,
+    'cache_limiter' => 'nocache',
+];
+
+// Logger settings.
 $settings['logger'] = [
     // Log file location
-    'path' => realpath(__DIR__ . '/../var/logs'),
+    'path' => realpath(__DIR__ . '/../../var/logs'),
     // Default log level
     'level' => Psr\Log\LogLevel::DEBUG,
 ];
 
-// Database settings
+// Database settings.
 $settings['db'] = [
     'host' => 'localhost',
     'encoding' => 'utf8mb4',
