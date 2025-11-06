408bef1879b500a0612cb9f324c1050eb447bc44
DavidSabb <158499251+DavidSabb@users.noreply.github.com>
2025-11-05 22:13:04 -0500

    updated sql file

--- VKC_db.sql
+++ VKC_db.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users(
 	user_id INT PRIMARY KEY AUTO_INCREMENT,
 	user_role ENUM('employee', 'admin') DEFAULT 'employee',
-	fist_name VARCHAR(50) NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
 	last_name VARCHAR(50) NOT NULL,
 	email VARCHAR(50) NOT NULL,
 	phone INT(10) NOT NULL,
@@ -12,19 +12,19 @@ CREATE TABLE users(
 
 CREATE TABLE shift(
     shift_id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    shift_start DATE NOT NULL,
-    shift_end DATE NOT NULL,
-    break_minutes INT,
-    shift_date DATE NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+    schedule_id INT NOT NULL,
+    shift_start DATETIME NOT NULL,
+    shift_end DATETIME NULL,
+    FOREIGN KEY (schedule_id) REFERENCES schedule(schedule_id),
 );
 
 CREATE TABLE schedule(
     schedule_id INT PRIMARY KEY AUTO_INCREMENT,
-    week_start DATE NOT NULL,
-    shift_id INT NOT NULL,
-    
+    user_id INT NOT NULL,
+    start_time DATETIME NOT NULL,
+    end_time DATETIME NOT NULL,
+    schedule_status ENUM('pending','confirmed','rejected') NOT NULL DEFAULT 'pending',
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
 );
 
 CREATE TABLE station(
@@ -34,18 +34,23 @@ CREATE TABLE station(
 
 CREATE TABLE team(
     team_id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT NOT NULL,
     station_id INT NOT NULL,
     team_creation_date DATE NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
-    FOREIGN KEY (station_id) REFERENCES station(station_id) ON DELETE CASCADE,
+    FOREIGN KEY (station_id) REFERENCES station(station_id),
+);
+
+CREATE TABLE team_members(
+    team_id INT NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (team_id) REFERENCES team(team_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
 );
 
 CREATE TABLE product(
     product_id INT PRIMARY KEY AUTO_INCREMENT,
-    sku VARCHAR(40),
+    product_category ENUM('SCI', 'U-BASE', '100-BASE'),
+    product_code VARCHAR(50) NOT NULL,
     product_name VARCHAR(50) NOT NULL,
-    product_category ENUM('resin', 'adhesive', 'solvent', 'coating') DEFAULT 'resin'
 );
 
 CREATE TABLE colour(
@@ -57,8 +62,9 @@ CREATE TABLE colour(
 CREATE TABLE product_variant(
     variant_id INT PRIMARY KEY AUTO_INCREMENT,
     product_id INT NOT NULL,
-    colour_id INT,
-    variant_description VARCHAR(30) NOT NULL,
+    colour_id INT NULL,
+    unit_size ENUM('0.5L', '1L', '2L', '4L', '8L'),
+    variant_description VARCHAR(50) NOT NULL,
     FOREIGN KEY (product_id) REFERENCES product(product_id),
     FOREIGN KEY (colour_id) REFERENCES colour(colour_id)
 );
@@ -67,7 +73,6 @@ CREATE TABLE tote(
     tote_id INT PRIMARY KEY AUTO_INCREMENT,
     variant_id INT NOT NULL,
     batch_number INT UNIQUE,
-    sci_product ENUM('SCI', 'U-BASE', '100-BASE'),
     FOREIGN KEY (variant_id) REFERENCES product_variant(variant_id)
 );
 
@@ -75,14 +80,19 @@ CREATE TABLE pallet(
     pallet_id INT PRIMARY AUTO_INCREMENT,
     tote_id INT NOT NULL,
     station_id INT NOT NULL,
-    unit_size ENUM('0.5L', '1L'),
-    start_time DATE NOT NULL,
-    end_time DATE NOT NULL,
-    units INT NOT NULL,
-    breaks BOOLEAN NOT NULL,
-    break_time INT,
-    mess BOOLEAN NOT NULL,
-    notes VARCHAR(100),
     FOREIGN KEY (tote_id) REFERENCES tote(tote_id),
-    FOREIGN KEY (station_id) REFERENCES station(station_id)
+    FOREIGN KEY (station_id) REFERENCES station(station_id),
+);
+
+CREATE TABLE palletize_session(
+    session_id INT PRIMARY KEY AUTO_INCREMENT,
+    pallet_id INT NOT NULL,
+    start_time DATE NOT NULL,
+    end_time DATE NULL,
+    units INT NULL,
+    breaks BOOLEAN NULL,
+    break_time INT NULL,
+    mess BOOLEAN NULL,
+    notes VARCHAR(100) NULL,
+    FOREIGN KEY (pallet_id) REFERENCES pallet(pallet_id),
 );
