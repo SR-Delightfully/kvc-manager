44be5905636e19844a5d117120dfdc12d50f8fed
markololo <markololo2468@gmail.com>
2025-11-18 14:11:34 -0500

    Edited KPImodel

--- app/Domain/Models/KpiModel.php
+++ app/Domain/Models/KpiModel.php
@@ -11,17 +11,47 @@ class KpiModel extends BaseModel
     }
 
     /**
-     * The function scans the progress of all teams and it
+     *      * The function scans the progress of all teams and it
      *  returns an array that represents the leaderboard of th teams, from best to worst performance. The performance is based on units produced per minute on average.
-     * The calculation only takes into account the records that match the input dates
+     * The calculation only takes into account the records that match the input dates     *
+     * @param mixed $startDate start date to start calculations
+     * @param mixed $endDate date of the last calculations
      * @return array the array's first index is to the most performant team and so on.
-     *
      */
-    public function getTeamLeaderboard($start, $end)
+    public function getTeamLeaderboard($startDate, $endDate) : array
     {
-        $stmt = "SELECT pallet.*, palletize_session.* FROM pallet LEFT JOIN palletize_session ON pallet.pallet_id =  palletize_session.pallet_id WHERE pallet.pallet_id = :id";
+        //? 1) Find all pelletize sessions in the date range
+        //? 2) Calculate the units/minute worked for every fund session
+        //? 3) Calculate the avg for the team
+        //? 4) Compare and sort
+        //? 5) return the result array
+
+        $stmt = "SELECT ";
         $params = [':id'=>$id];
-        $pallet = $this->selectOne($stmt,$params);
+        $sessions = $this->selectOne($stmt,$params);
         return [];
+    }
+
+    //! teams have names or colors or just ID? Maybe update the db to add team names/color codes
+
+    public function getTeamPerformance($teamId, $startDate, $endDate) : array
+    {
+        //? 1) Get all the pelletize sessions of the team
+        //? 2) Filter only the sessions from the time frame
+        //? 3) Get the productivity of each session (units produced per minute)
+        //? 4) Place the results in an array in chronological order
+
+        return [];
+    }
+
+    /**
+     * Calculates the 
+     * @param mixed $date
+     * @return int
+     */
+    public function getTeamDailyProduction($date) : int
+    {
+
+        return 300;
     }
 }
